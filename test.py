9c13f3967dec2da6478aae0230cbef4c3d2f911a
2k9u1 <gwlee1098@naver.com>
2025-02-19 21:33:27 +0900

    test.py 에 food 변수 추가가

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("hello gyu!" + str(12345))
+
+# create a varible of foods and print it
+foods = ["apple", "banana", "cherry"]
+print(foods)
+
+# create a function that takes a list of foods and prints it
+def print_foods(foods):
+    for food in foods:
+        print(food)
